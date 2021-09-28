14cd9725f96228ac36fe40122dee1e66d7c3782f
maxilong <adc3080027554@gmail.com>
2021-09-28 16:22:47 +0800

    xixix

--- src/42接雨水.ts
+++ src/42接雨水.ts
@@ -14,3 +14,30 @@ function trap(height: number[]): number {
   }
   return res
 };
+
+//双指针做法
+// function trap(height: number[]): number {
+//   let i = 0
+//   let j = height.length - 1
+//   let res = 0
+//   let leftMax = 0
+//   let rightMax = 0
+//   while (i < j) {
+//     if (height[i] < height[j]) {
+//       if (height[i] >= leftMax) {
+//         leftMax = height[i]
+//       } else {
+//         res += (leftMax - height[i])
+//       }
+//       i++
+//     } else {
+//       if (height[j] >= rightMax) {
+//         rightMax = height[j]
+//       } else {
+//         res += (rightMax - height[j])
+//       }
+//       j--
+//     }
+//   }
+//   return res
+// };
