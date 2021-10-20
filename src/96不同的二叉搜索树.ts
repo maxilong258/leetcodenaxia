33df05d9577d2fd98f95db34f9b02596a46565a3
maxilong <adc3080027554@gmail.com>
2021-10-20 17:01:18 +0800

    yeyey

--- src/96不同的二叉搜索树.ts
+++ src/96不同的二叉搜索树.ts
@@ -1,16 +1,16 @@
 function numTrees(n: number): number {
-  const memo = new Array(n + 1).fill(null)
+  const dp = new Array(n + 1).fill(null)
   return dfs(n)
   function dfs(n: number): number {
     if (n <= 1) return 1
-    if (memo[n] !== null) return memo[n]
+    if (dp[n] !== null) return dp[n]
     let res = 0
     for (let i = 1; i <= n; i++) {
       let left = dfs(i - 1)
       let right = dfs(n - i)
       res += left * right
     }
-    memo[n] = res
+    dp[n] = res
     return res
   }
 }
